5c11e03d474afc6cb95c8c15b119a2bd39b18dd6
samw060 <sam.wilkinson935@gmail.com>
2026-08-19 21:29:21 +0100

    Create or opens a database file

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,11 @@
+import java.io.File;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args){
         // First ticket
         System.out.println("Hello");
+        FileManager manager = new FileManager();
     }
 }
