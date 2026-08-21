a4220975f869d4d123cae7569c5314827424010a
samw060 <sam.wilkinson935@gmail.com>
2026-08-21 22:19:59 +0100

    SE-14 Frame array, page table, free list and frame meta data

--- src/BufferPool.java
+++ src/BufferPool.java
@@ -1,17 +1,28 @@
-import java.awt.*;
 import java.nio.ByteBuffer;
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.HashMap;
 import java.util.Map;
 
 public class BufferPool {
+    private FileManager fileManager;
     private Frame[] frames;
     private Map<Integer, Integer> pageTable;
-    private boolean[] freeList;
+    private Deque<Integer> freeList;
+    private int POOL_SIZE;
 
     public BufferPool(){
-        frames = new Frame[5];
-        pageTable = new HashMap<>();
-        freeList = new boolean[5];
+        this.POOL_SIZE = 5;
+        this.fileManager = new FileManager();
+        this.frames = new Frame[POOL_SIZE];
+        this.pageTable = new HashMap<>();
+        this.freeList = new ArrayDeque<>();
+
+        for (int i = 0; i < POOL_SIZE; i++) {
+            frames[i] = new Frame(ByteBuffer.allocate(FileManager.PAGE_SIZE));
+            freeList.add(i);
+        }
     }
+
 
 }
